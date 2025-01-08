a8b49b0eb05964c3ff94b0248afdd7574cf6b7b2
eChemmara <echemmara@gmail.com>
2025-01-09 05:23:56 +0800

    AddProduct.js

---
+++ AddProduct.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductForm from './ProductForm';
+
+const AddProduct = () => {
+    return (
+        <div className="add-product">
+            <h2>Add New Product</h2>
+            <ProductForm />
+        </div>
+    );
+};
+
+export default AddProduct;
